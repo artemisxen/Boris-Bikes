bc788a4013c715bf0d484d4bdda41694bd1e7f66
Ryan Davidson <daviryan@aol.com>
2017-07-04 15:47:00 +0100

    challenge 11

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,12 +1,18 @@
 require 'bike'
 class DockingStation
-
+	attr_reader :bike
 	def initialize
-
+		#@bikes = []
+		@bike = nil
 	end
 
 	def release_bike
 		Bike.new
+	end
+
+	def dock(bike)
+		#@bikes.push(bike)
+		@bike = bike
 	end
 
 end
--- spec/dockingStation_spec.rb
+++ spec/dockingStation_spec.rb
@@ -8,4 +8,9 @@ describe DockingStation do
 	it "DockingStation to return a bike after release_bike called" do
 		expect(subject.release_bike).to be_a Bike
 	end
+	it "DockingStation to dock a bike" do
+		bike = Bike.new
+		expect(subject).to respond_to(:dock)
+		expect(subject.dock(bike)).to eq bike
+	end
 end
