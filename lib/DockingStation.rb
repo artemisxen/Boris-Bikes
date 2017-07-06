6ea3513f465e2f21fb0358bc8a563687629a0fbf
artemis <artemisxen@gmail.com>
2017-07-06 14:55:25 +0100

    challenge 15

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -8,13 +8,21 @@ class DockingStation
 
 	def release_bike
 		#Bike.new
-		raise "No bikes available to release" if @bikes == []
+		raise "No bikes available to release" if empty?
 		@bikes.pop
 	end
 
 	def dock(bike)
-		raise "Docking Station full" if @bikes.length >= 20
+		raise "Docking Station full" if full?
 		@bikes << bike
 	end
 
+	private
+	def full?
+		@bikes.length == 20 ? true : false
+	end
+
+	def empty?
+		@bikes.length == 0 ? true : false
+	end
 end
